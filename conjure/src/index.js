4ee1db34304cca4fb977fb3839727934d1eb6343
HexaField <joshfield999@gmail.com>
2020-11-27 10:21:29 +1100

    server now runs in node environment

--- conjure/src/index.js
+++ conjure/src/index.js
@@ -5,34 +5,40 @@ import { getParams } from './urldecoder.js'
 
 async function startApp() {
 
-    const worldSync = await WorldSync({ 
-        // serverFile: '_dist_/start.js', 
-        serverFunc: runApp,
-        canvas: isNode ? undefined : document.getElementById('canvas'),
-        config: {
-            assetSync: false,
-            urlParams: getParams(location.href),
-            touchDevice: Boolean('ontouchstart' in window)
+    if(isNode) {
+
+        runApp()
+
+    } else {
+
+        const worldSync = await WorldSync({ 
+            // serverFile: '_dist_/start.js', 
+            serverFunc: runApp,
+            canvas: document.getElementById('canvas'),
+            config: {
+                assetSync: false,
+                urlParams: getParams(location.href),
+                touchDevice: Boolean('ontouchstart' in window)
+            }
+        })
+        if(worldSync._isRunningInWorker) {
+
+            const { MediaHandler } = await import('./media/MediaHandler.js')
+            const mediaHandler = new MediaHandler(worldSync)
+
+            worldSync._peerSync.addEventListener('requestPointerLock', () => { 
+                worldSync._peerSync.canvas.requestPointerLock()
+            })
+            worldSync._peerSync.addEventListener('exitPointerLock', () => { 
+            document.exitPointerLock()
+            })
+            worldSync._peerSync.addEventListener('open', (...args) => { 
+            window.open(...args)
+            })
+            worldSync._peerSync.addRequestOpcode('media', async (args) => {
+                return await mediaHandler.handle(args)
+            })
         }
-    })
-    if(worldSync._isRunningInWorker) {
-
-        const { MediaHandler } = await import('./media/MediaHandler.js')
-        const mediaHandler = new MediaHandler(worldSync)
-
-        worldSync._peerSync.addEventListener('requestPointerLock', () => { 
-            worldSync._peerSync.canvas.requestPointerLock()
-        })
-        worldSync._peerSync.addEventListener('exitPointerLock', () => { 
-           document.exitPointerLock()
-        })
-        worldSync._peerSync.addEventListener('open', (...args) => { 
-           window.open(...args)
-        })
-        worldSync._peerSync.addRequestOpcode('media', async (args) => {
-            return await mediaHandler.handle(args)
-        })
-        
     }
 }
 startApp()
